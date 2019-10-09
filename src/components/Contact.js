db01c573e5bb4ed9e74824dc5eb17c6a67bf6b06
Mary Do <marydo318@gmail.com>
2019-10-08 23:43:23 -0700

    initial commit

---
+++ src/components/Contact.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import propTypes from "prop-types";
+
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  onShowClick = e => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  };
+
+  render() {
+    const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
+    return (
+      <div className="card card-body mb-3">
+        <h4>
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down"></i>
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">{email}</li>
+            <li className="list-group-item">{phone}</li>
+          </ul>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+Contact.propTypes = {
+  contact: propTypes.object.isRequired
+};
+
+export default Contact;
